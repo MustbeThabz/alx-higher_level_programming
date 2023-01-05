41e39d3e278e4312b2934d5ffae1f81b02babc79
MustbeThabz <tebogodipalett@gmail.com>
2023-01-05 13:34:33 +0000

    Task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,29 +6,22 @@
  *
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-typedef struct listint_s {
-    int n;
-    struct listint_s *next;
-} listint_t;
+	if (!list)
+		return (0);
 
-int check_cycle(listint_t *list) {
-    if (list == NULL) {
-        return 0;
-    }
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
 
-    listint_t *slow = list;
-    listint_t *fast = list;
-
-    while (fast != NULL && fast->next != NULL) {
-        slow = slow->next;
-        fast = fast->next->next;
-
-        if (slow == fast) {
-            return 1;
-        }
-    }
-
-    return 0;
+	return (0);
 }
 

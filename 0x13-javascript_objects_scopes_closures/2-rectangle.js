3874a99c52092d3c1fd49b38576991c4d4efdd35
MustbeThabz <tebogodipalett@gmail.com>
2023-04-12 23:07:11 +0000

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,19 +1,13 @@
 #!/user/bin/node
 
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <=) {
-     return{};
+  contstructor(w, h) {
+    if (w <= 0 || h <= 0) {
+      return ()
     }
     this.width = w;
     this.height = h;
    }
-
-    print() {
-      for (let i = 0; i < this.height; i++) {
-	console.log("X".repeat(this.width));
-       }
-     }
-   }
+ }
 
 module.exports = Rectangle;

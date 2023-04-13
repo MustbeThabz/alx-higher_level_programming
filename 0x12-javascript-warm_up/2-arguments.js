b064113a6da40e9ae9ee547ef35c49fb974a2e13
MustbeThabz <tebogodipalett@gmail.com>
2023-04-13 15:08:10 +0000

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-const numArgs = args.length;
+const numArgs = process.argv.length - 2;
 
-if (numArgs === 0) {
-  console.log('No argument');
-} else if (numArgs === 1) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
+switch (numArgs) {
+  case 0:
+    console.log('No argument');
+    break;
+  case 1:
+    console.log('Argument found');
+    break;
+  default:
+    console.log('Arguments found');
 }
+
 

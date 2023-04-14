8dee2f14aa805915b016400f80bd6fb04b68aa94
MustbeThabz <tebogodipalett@gmail.com>
2023-04-14 02:52:22 +0000

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
 const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let i = 0;
 
-for (let i = 0; i < languages.length; i++) {
+while (i < languages.length) {
   console.log(languages[i]);
+  i++;
 }

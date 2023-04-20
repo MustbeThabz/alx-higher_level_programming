16a8d90ae6ba25c59e8f125e326a9fde8af82c6c
MustbeThabz <tebogodipalett@gmail.com>
2023-04-20 07:22:28 +0000

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,8 @@
-!#/usr/bin/node
 
-function add(a, b) {
+#!/usr/bin/node
+function add (a, b) {
   const sum = a + b;
   console.log(sum);
 }
 
-
-add(a, b);
+add(Number(process.argv[2]), Number(process.argv[3]));
